2a1f3415892fca7d018dab6a9d36fe305b31dd1f
Pradeep Detroja <detrojapradeep111@gmail.com>
2025-01-09 20:22:03 +0000

    mock data replaced by live data from swiggy API, re-rendering using useEffect

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,20 +1,36 @@
 import RestaurantCard from "./RestaurantCard";
-import restObj from "../utils/mockData";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 // IMP while using MAPS : No Keys <<<<<< Indexes as keys <<<<< Unique ids as keys
 
 const Body = () => {
-  const [restList, setRestList] = useState(restObj);
+  const [restList, setRestList] = useState([]);
 
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const jsonData = await data.json();
+    // console.log(jsonData);
+    setRestList(
+      jsonData.data.cards[1].card.card.gridElements.infoWithStyle.restaurants
+    );
+  };
   return (
     <div className="Body">
       <div className="filter">
         <button
           className="filter-btn"
           onClick={() => {
-            const filteredList = restList.filter(
-              (res) => res.info.avgRating > 4.5
-            );
+            const filteredList = restList
+              .filter((res) => res.info.avgRating > 4)
+              .sort(
+                (a, b) =>
+                  parseFloat(b.info.avgRating) - parseFloat(a.info.avgRating)
+              );
             setRestList(filteredList);
           }}
         >
